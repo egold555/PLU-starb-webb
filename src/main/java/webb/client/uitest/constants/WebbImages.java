54451b07507e1902fc413323d8265e5ceb8e86b0
Eric Golde <eric@golde.org>
2023-03-11 10:36:14 -0800

    Created image loader

---
+++ src/main/java/webb/client/uitest/constants/WebbImages.java
@@ -0,0 +1,53 @@
+package webb.client.uitest.constants;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
+
+public class WebbImages {
+
+    private static final BufferedImage ERROR_IMAGE = createErrorImage(300, 300);
+
+    public static final BufferedImage TEST = loadImage("test.png");
+    public static final BufferedImage NOT_FOUND = loadImage("notfound.png");
+
+    private static BufferedImage loadImage(String imageName) {
+        final String imagePath = "/webb/images/" + imageName;
+        try {
+            InputStream is = WebbImages.class.getResourceAsStream(imagePath);
+            if(is == null) {
+                System.err.println("Could not find image: " + imagePath);
+                return ERROR_IMAGE;
+            }
+
+            return ImageIO.read(is);
+        }
+        catch(Exception e) {
+            System.err.println("Could not load image: " + imagePath);
+            e.printStackTrace();
+            return ERROR_IMAGE;
+        }
+
+    }
+
+    //create pink and black checkerboard image
+    private static BufferedImage createErrorImage(int width, int height) {
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Color c1 = new Color(255, 105, 180);
+        Color c2 = Color.BLACK;
+
+        for(int x = 0; x < width; x++) {
+            for(int y = 0; y < height; y++) {
+                if((x + y) % 2 == 0) {
+                    image.setRGB(x, y, c1.getRGB());
+                }
+                else {
+                    image.setRGB(x, y, c2.getRGB());
+                }
+            }
+        }
+
+        return image;
+    }
+}
