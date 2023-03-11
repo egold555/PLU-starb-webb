9675580fd2f566dca5c77d0baaa5a2e0d05cba77
Eric Golde <eric@golde.org>
2023-03-11 11:53:28 -0800

    Color constants for UI

---
+++ src/main/java/webb/client/uitest/constants/WebbColors.java
@@ -0,0 +1,12 @@
+package webb.client.uitest.constants;
+
+import java.awt.Color;
+public class WebbColors {
+
+    public static final Color B7 = Color.decode("#B7B7B7");
+    public static final Color D9 = Color.decode("#D9D9D9");
+
+    public static final Color TEXT_COLOR_WHITE = Color.WHITE;
+    public static final Color TEXT_COLOR_BLACK = Color.BLACK;
+
+}
