59df402a3d9059e7ab38e698d54f77aa9fbd7673
Eric Golde <eric@golde.org>
2023-03-23 20:57:22 -0700

    Leaderboard score now compares time if scores are the same. If scores and time are somehow the same, it sorta A-Z

--- src/main/java/webb/client/ui/popup/leaderboard/LeaderboardScore.java
+++ src/main/java/webb/client/ui/popup/leaderboard/LeaderboardScore.java
@@ -7,16 +7,24 @@ public class LeaderboardScore implements Comparable<LeaderboardScore> {
 
     private final String name;
     private final int score;
+    private final long time;
+
+    @Deprecated
+    public LeaderboardScore(String name, int score) {
+        this(name, score, 0);
+    }
 
     /**
      * Creates a new LeaderboardScore.
      *
      * @param name  The name of the player
      * @param score The score of the player
+     * @param time The completion time of the player (in milliseconds)
      */
-    public LeaderboardScore(String name, int score) {
+    public LeaderboardScore(String name, int score, long time) {
         this.name = name;
         this.score = score;
+        this.time = time;
     }
 
     /**
@@ -34,14 +42,33 @@ public class LeaderboardScore implements Comparable<LeaderboardScore> {
     public int getScore() {return score;}
 
     /**
+     * Gets the completion time of the player.
+     *
+     * @return The completion time of the player (in milliseconds)
+     */
+    public long getTime() {return time;}
+
+    /**
      * Compares this LeaderboardScore to another LeaderboardScore.
      * This sorts from highest to lowest.
+     * If scores are equal, it sorts the time from highest to lowest.
+     * If some miracle happens and scores and times are equal, it sorts by name from A to Z.
      *
      * @param o The other LeaderboardScore
      * @return 1 if this LeaderboardScore is greater than the other LeaderboardScore, -1 if this LeaderboardScore is less than the other LeaderboardScore, and 0 if they are equal
      */
     @Override
     public int compareTo(LeaderboardScore o) {
-        return Integer.compare(o.score, this.score);
+
+        // Sort by score, High to Low
+        final int scoreCompare = Integer.compare(o.score, this.score);
+        if(scoreCompare != 0) return scoreCompare;
+
+        // If scores are equal, sort by time, Low to High
+        final int timeCompare = Long.compare(this.time, o.time);
+        if(timeCompare != 0) return timeCompare;
+
+        // If scores and times are equal, sort by name, A to Z
+        return this.name.compareTo(o.name);
     }
 }
