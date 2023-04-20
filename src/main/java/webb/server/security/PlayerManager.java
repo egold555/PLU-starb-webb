e1bbd0469dd4e967cfa9bfe6425aacacebba83ea
Eric Golde <eric@golde.org>
2023-04-20 12:34:59 -0700

    User authenticates with my mock server now

--- src/main/java/webb/server/security/PlayerManager.java
+++ src/main/java/webb/server/security/PlayerManager.java
@@ -1,16 +1,31 @@
 package webb.server.security;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import webb.client.model.Player;
+import webb.client.ui.helpers.WebbWebUtilities;
 import webb.server.repository.MockPlayerDatabase;
 
 public class PlayerManager {
-    // this'll eventually disappear and be a dependency injection
-    private static final MockPlayerDatabase users = new MockPlayerDatabase();
 
     private static Player currentPlayer;
 
     public static boolean authenticate(Player player)  {
-        if(users.exists(player.username())) {
+
+        System.out.println("Authenticating...");
+
+        // Check if the player exists. Right now the server just checks the same userdata file as we had before
+        // Just returns "exists" as a boolean if the user was in that file or not
+        ObjectNode node = WebbWebUtilities.getRequest("does-user-exist?username=" + player.username());
+
+        //If we failed to connect to the server, we can't authenticate
+        if(!node.get("success").asBoolean()) {
+            System.out.println("Failed to authenticate!");
+            System.out.println(node.get("error").asText());
+            return false;
+        }
+
+        //If the player doesn't exist, we can't authenticate
+        if(!node.get("data").get("exists").asBoolean()) {
             return false;
         }
 
