5efda75f8c62150040b1b5a5ed77a74b78ffbd80
chrishollandaise <chris@chrishollandaise.com>
2023-04-21 14:03:29 -0700

    chore(server): mark MockPlayerDatabase as deprecated

--- src/main/java/webb/server/repository/MockPlayerDatabase.java
+++ src/main/java/webb/server/repository/MockPlayerDatabase.java
@@ -6,6 +6,7 @@ import webb.client.model.Player;
 import java.io.File;
 import java.util.*;
 
+@Deprecated
 public class MockPlayerDatabase {
     private final Map<String, Player> players = new HashMap<>();
     private final String FP = "src/main/resources/webb/data/userdata.json";
@@ -16,7 +17,7 @@ public class MockPlayerDatabase {
         File file = new File(FP);
 
         try {
-            List<Player> playerList = Arrays.asList(mapper.readValue(file, Player[].class));
+            Player[] playerList = mapper.readValue(file, Player[].class);
 
             for(Player player : playerList) {
                 players.put(player.username(), player);
