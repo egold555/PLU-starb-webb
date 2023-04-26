c932d800ae9e2a22fdb289f7ec3eac68564388ba
chrishollandaise <chris@chrishollandaise.com>
2023-04-26 13:46:27 -0700

    leaderboard uses userService

--- src/main/java/webb/server/controller/LeaderboardController.java
+++ src/main/java/webb/server/controller/LeaderboardController.java
@@ -1,46 +1,43 @@
 package webb.server.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import webb.server.repository.UserRepository;
+import webb.server.service.UserService;
 import webb.shared.dtos.leaderboard.LeaderboardDTO;
-import webb.shared.dtos.leaderboard.LeaderboardEntryDTO;
-import webb.shared.dtos.user.UserDTO;
-import webb.shared.dtos.user.UserDTOComparator;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.security.Principal;
 
 @RestController
 @RequestMapping(path="leaderboards")
+@CrossOrigin("*")
 public class LeaderboardController {
     /**
-     * Repository object for managing User entities.
+     * service for managing users
      */
-    private final UserRepository userRepo;
+    private final UserService userService;
 
     /**
      * Constructor for UserController class.
-     * @param userRepo Repository object for managing User entities.
+     *
+     * @param userService
      */
     @Autowired
-    public LeaderboardController(UserRepository userRepo) {
-        this.userRepo = userRepo;
+    public LeaderboardController(UserService userService) {
+        this.userService = userService;
     }
 
+    /**
+     * Endpoint for retrieving all User entities.
+     * @return List of all User entities
+     */
     @GetMapping("users")
     public ResponseEntity<LeaderboardDTO> getUserLeaderboard() {
-        List<UserDTO> users = userRepo.findAll();
-        users.sort(new UserDTOComparator());
-        List<LeaderboardEntryDTO> entries = users.stream()
-                .map(user -> new LeaderboardEntryDTO(user.getUsername(), user.getStats().getPuzzlesComplete()))
-                .toList();
-        return new ResponseEntity<>(new LeaderboardDTO(entries), HttpStatus.OK);
+        return new ResponseEntity<>(userService.getUserLeaderboard(), HttpStatus.OK);
     }
 }
